374d4eabfd32849267e55c32bcf7e2cbd77bba09
Catalin Ionescu <cionescu+github@gmail.com>
2022-02-07 19:41:50 +0200

    Better main menu

--- src/com/catalinionescu/miniscript/Main.java
+++ src/com/catalinionescu/miniscript/Main.java
@@ -4,13 +4,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+import com.catalinionescu.miniscript.exceptions.LexerException;
+
+public class Main {	
+	public void printMenu() {
+		System.out.println();
+		System.out.println("Menu: [1] Lexer Unit Tests  [2] Parser Unit Tests  [3] REPL           [4] Exit");
+		System.out.print("> ");
+	}
 
 	public static void main(String[] args) throws IOException {
-		Interpreter repl = new Interpreter();
-		repl.implicitOutput = repl.getStandardOutput();
+		Main main = new Main();
+		main.run();
+	}
+
+	private void run() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader input = new BufferedReader(isr);
+		
+		while (true) {
+			printMenu();
+			String menu = input.readLine().trim();
+			
+			switch (menu) {
+				case "1":
+					runLexerTests();
+					break;
+				case "2":
+					runParserTests();
+					break;
+				case "3":
+					runREPL(input);
+					break;
+				case "4":
+					System.out.println();
+					System.out.println("Bye!");
+					return;
+			}
+		}
+	}
+
+	private void runREPL(BufferedReader input) throws IOException {
+		System.out.println("*****************************************************************************");
+		System.out.println("Running REPL");
+		System.out.println();
+
+		Interpreter repl = new Interpreter();
+		repl.implicitOutput = repl.getStandardOutput();
 		
 		while (true) {
 			System.out.print(repl.NeedMoreInput() ? ">>> " : "> ");
@@ -20,4 +60,24 @@ public class Main {
 		}
 	}
 
+	private void runParserTests() {
+		System.out.println("*****************************************************************************");
+		System.out.println("Running PARSER tests");
+		System.out.println();
+
+		Parser.RunUnitTests();
+	}
+
+	private void runLexerTests() {		
+		System.out.println("*****************************************************************************");
+		System.out.println("Running LEXER tests");
+		System.out.println();
+		
+		try {
+			Lexer.RunUnitTests();
+		} catch (LexerException e) {
+			System.err.println("Lexer generated an exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
